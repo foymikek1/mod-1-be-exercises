06a684e227688cc68209e36cd57dc17763823231
foymikek <foymikek@gmail.com>
2025-02-27 15:57:28 -0700

    Add passing week 1

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,18 @@
 class Venue
+  attr_reader :name
+  attr_accessor :capacity, :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    @patrons.map {|patron| patron.upcase }
+  end
 end
